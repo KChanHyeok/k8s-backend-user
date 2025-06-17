aa39593d77ce4afa355e8e79bb34a2052411bb92
강찬혁 <72860075+KChanHyeok@users.noreply.github.com>
2025-06-17 14:02:20 +0900

    원상복구

--- src/main/java/com/welab/k8s_backend_user/advice/ApiCommonAdvice.java
+++ src/main/java/com/welab/k8s_backend_user/advice/ApiCommonAdvice.java
@@ -1,6 +1,5 @@
 package com.welab.k8s_backend_user.advice;
 
-
 import com.welab.k8s_backend_user.advice.parameter.ParameterErrorDto;
 import com.welab.k8s_backend_user.common.dto.ApiResponseDto;
 import com.welab.k8s_backend_user.common.exception.BadParameter;
@@ -23,6 +22,8 @@ public class ApiCommonAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({BadParameter.class})
     public ApiResponseDto<String> handleBadParameter(BadParameter e) {
+        e.printStackTrace();
+
         return ApiResponseDto.createError(
                 e.getErrorCode(),
                 e.getErrorMessage()
@@ -32,6 +33,8 @@ public class ApiCommonAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({NotFound.class})
     public ApiResponseDto<String> handleNotFound(NotFound e) {
+        e.printStackTrace();
+
         return ApiResponseDto.createError(
                 e.getErrorCode(),
                 e.getErrorMessage()
@@ -41,33 +44,41 @@ public class ApiCommonAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({ClientError.class})
     public ApiResponseDto<String> handleClientError(ClientError e) {
+        e.printStackTrace();
+
         return ApiResponseDto.createError(
                 e.getErrorCode(),
                 e.getErrorMessage()
         );
     }
 
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ExceptionHandler({NoResourceFoundException.class})
-//    public ApiResponseDto<String> handleNoResourceFoundException(NoResourceFoundException e) {
-//        return ApiResponseDto.createError(
-//                "NoResource",
-//                "리소스를 찾을 수 없습니다.");
-//    }
-//
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler({MethodArgumentNotValidException.class})
-//    public ApiResponseDto<ParameterErrorDto.FieldList> handleArgumentNotValidException(MethodArgumentNotValidException e) {
-//        BindingResult result = e.getBindingResult();
-//        ParameterErrorDto.FieldList fieldList = ParameterErrorDto.FieldList.of(result);
-//
-//        String errorMessage = fieldList.getErrorMessage();
-//        return ApiResponseDto.createError("ParameterNotValid", errorMessage, fieldList);
-//    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({NoResourceFoundException.class})
+    public ApiResponseDto<String> handleNoResourceFoundException(NoResourceFoundException e) {
+        e.printStackTrace();
+
+        return ApiResponseDto.createError(
+                "NoResource",
+                "리소스를 찾을 수 없습니다.");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ApiResponseDto<ParameterErrorDto.FieldList> handleArgumentNotValidException(MethodArgumentNotValidException e) {
+        e.printStackTrace();
+
+        BindingResult result = e.getBindingResult();
+        ParameterErrorDto.FieldList fieldList = ParameterErrorDto.FieldList.of(result);
+
+        String errorMessage = fieldList.getErrorMessage();
+        return ApiResponseDto.createError("ParameterNotValid", errorMessage, fieldList);
+    }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler({Exception.class})
     public ApiResponseDto<String > handleException(Exception e) {
+        e.printStackTrace();
+
         return ApiResponseDto.createError(
                 "ServerError",
                 "서버 에러입니다.");
